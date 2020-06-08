50f14f6c9eafaa4cd3686ea3b9c87e9253bf27b6
Guilherme Zago <contatogzii@gmail.com>
2020-06-08 12:40:53 -0300

    Updates

---
+++ precache-manifest.194571c89194026d13338828106aa924.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a79ed2c636360aae2813179297573f39",
+    "url": "/hello-world-app/index.html"
+  },
+  {
+    "revision": "f2f61bba03418da84b92",
+    "url": "/hello-world-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "36fb853b09e8c9b74812",
+    "url": "/hello-world-app/static/js/2.be7e122f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hello-world-app/static/js/2.be7e122f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2f61bba03418da84b92",
+    "url": "/hello-world-app/static/js/main.aa12c631.chunk.js"
+  },
+  {
+    "revision": "68ad317f884dd8d441be",
+    "url": "/hello-world-app/static/js/runtime-main.a37e3e3a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hello-world-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
